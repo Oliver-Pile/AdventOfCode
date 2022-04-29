10f5e72be3ed54123b88b318a07e1718af328f79
Oliver-Pile <31112606+Oliver-Pile@users.noreply.github.com>
2022-04-29 14:03:55 +0100

    Advent of Code - Day 1.

---
+++ day1.py
@@ -0,0 +1,31 @@
+import math
+def part1(fileName):
+    file = open(fileName,"r")
+    prev = math.inf 
+    increased = 0
+    for line in file:
+        if(int(line)>prev):
+            increased+=1
+        prev = int(line)
+    file.close()
+    return increased
+
+print(part1("AdventOfCode/day1.txt"))
+
+def part2(fileName):
+    file = open(fileName,"r")
+    i = 0
+    windows = [0,0,0]
+    prev = math.inf
+    increased = 0
+    for line in file:
+        windows[i % 3] = int(line)
+        if (i>3):
+            if(sum(windows)>prev):
+                increased +=1
+            prev = sum(windows)
+        i+=1
+    file.close()
+    return increased
+
+#print(part2("day1.txt"))
